e7a3a5aa7fabf5c31d0f2ade236e63c8dde2bb84
4-Endless-coder <asheshdash4@gmail.com>
2025-07-06 11:26:09 +0530

    useEffect in Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(0);
+
+    useEffect(()=> {
+    // callOnce();
+     counterFunc();
+  },[counter]);
+  
+
+  function callOnce() {
+    console.log('This function should only be called once');
+    
+  }
+
+ 
+  function counterFunc() {
+    console.log("TEST",counter);
+    
+  }
+
+
   return (
     <div>
       <h1>Hooks</h1>
+      <button onClick={()=>setCounter(counter+1)}>Counter{counter}</button>
+      <button onClick={()=>setData(data+1)}>Data{data}</button>
     </div>
   )
 }
