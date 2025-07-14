ecde9f6f1e2b16a05bde6d3aecc35fba237875d2
4-Endless-coder <asheshdash4@gmail.com>
2025-07-14 21:22:11 +0530

    Inline Style in react with profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,141 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const cardStyle = {
+    border: "3px solid #cccccc4f",
+    width: "300px",
+    boxShadow: "1px 2px 3px 0px #cccccc59",
+    padding: "10px",
+  };
   return (
-    <div>
-      <h1>Inline Style in React</h1>
-    </div>
-  )
-}
+    <>
+      <h1
+        style={{
+          color: "#FFCB61",
+          backgroundColor: "#000",
+          border: "2px solid black",
+          borderRadius: "15px",
+          width: "300px",
+          margin: "10px",
+        }}
+      >
+        Inline Style in React
+      </h1>
+      <div style={{display:'flex', flexWrap:'wrap',}}>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "300px" }}
+            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Ashesh Dash</h2>
+            <h3>Software Developer</h3>
+          </div>
+        </div>
 
-export default App
+      </div>
+    </>
+  );
+};
+
+export default App;
