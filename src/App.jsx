70515cf3156f2d0b8bdcc9e25a59bfa304f1edfb
4-Endless-coder <asheshdash4@gmail.com>
2025-07-15 10:50:04 +0530

    External Style with user card

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,242 @@
-import React, { useState } from "react";
+import React from "react";
+import "./CSS/Style.css";
 
 const App = () => {
-  const [cardStyle,setCardStyle] = useState(
-    {
-    border: "3px solid #cccccc4f",
-    width: "300px",
-    boxShadow: "1px 2px 3px 0px #cccccc59",
-  }
-)
-const [textColor, setTextColor] = useState('black')
-const [grid, setGrid] = useState(true)
-
-const updateTheme =(bgColor, textColor)=>{
-   setCardStyle({...cardStyle, backgroundColor:bgColor})
-   setTextColor(textColor)
-}
   return (
     <>
-      <h1
-        style={{
-          color: "#FFCB61",
-          backgroundColor: "#000",
-          border: "2px solid black",
-          borderRadius: "15px",
-          width: "300px",
-          margin: "10px",
-        }}
-      >
-        Inline Style in React
-      </h1>
-      <button onClick={()=>updateTheme('#ccc', 'green')}>Gray Theme</button>
-      <button onClick={()=>updateTheme('white', 'black')}>Default Theme</button>
-      <button onClick={()=>setGrid(!grid)}>Toggle Grid</button>  
-      <div style={{display: grid?'flex':'block' , flexWrap:'wrap',}}>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+      <h1 className="heading">External Css</h1>
+      <div className="container">
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div><div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "300px" }}
-            src="https://i.pinimg.com/736x/04/5e/aa/045eaa768043a76cb8d18bf3efd424ca.jpg"
-            alt=""
-          />
-          <div style={{padding: "10px",color:textColor}}>
-            <h2>Ashesh Dash</h2>
-            <h3>Software Developer</h3>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div className="user-card">
+          <div>
+            <img
+              className="img-style"
+              src="https://i.pinimg.com/736x/5b/eb/ce/5bebced2c589211f5e452ad32fff4dcf.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text-wrap">
+            <h1>Ashesh Dash</h1>
+            <p>Software Developer</p>
           </div>
         </div>
       </div>
