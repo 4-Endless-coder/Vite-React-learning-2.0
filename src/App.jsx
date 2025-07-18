934be7ea40bfc2a034833ec8efbbade7aa4eb9a6
4-Endless-coder <asheshdash4@gmail.com>
2025-07-18 23:25:18 +0530

    Styled Components with eact js

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import UserProfile from './UserProfile'
+import styled from 'styled-components'
 
 const App = () => {
+
+  // const Heading =styled.h1`
+  // color:red;
+  // border: 1px solid green;
+  // border-radius: 5px;
+  // margin:10px;
+  // padding:20px;
+  // `
+
+  const Heading=styled.h1({
+  color:'red',
+  border: '1px solid green',
+  borderRadius: '5px',
+  margin:'10px',
+  padding:'20px',
+  })
+
+  const StyleBtn=styled.button`
+  color:red;
+  width:130px;
+  height:40px;
+  margin:10px;
+  `
   return (
     <div>
-      <h1 className='heading'>Style with CSS Module</h1>
-      <div style={{display:'flex', flexWrap:'wrap'}}>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      </div>
+    <h1>Styled Component with React js</h1>
+    <Heading>Hello Heading</Heading>
+    <Heading>Hello Heading 2</Heading>
+    <Heading>Hello Heading 3</Heading>
+    <Heading>Hello Heading 4</Heading>
+    <StyleBtn>Login</StyleBtn>
+    <StyleBtn>Sgin Up</StyleBtn>
+    <StyleBtn>Back</StyleBtn>
     </div>
   )
 }
