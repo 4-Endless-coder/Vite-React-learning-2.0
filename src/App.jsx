e0a8ee89594946bd06601b8d0d3b8f49373e01de
4-Endless-coder <asheshdash4@gmail.com>
2025-07-19 23:58:49 +0530

    Bootstrap installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
-import styled from 'styled-components'
+import Button from 'react-bootstrap/Button';
+import { Alert, Navbar, Container, NavDropdown, Nav} from 'react-bootstrap';
 
 const App = () => {
-
-  // const Heading =styled.h1`
-  // color:red;
-  // border: 1px solid green;
-  // border-radius: 5px;
-  // margin:10px;
-  // padding:20px;
-  // `
-
-  const Heading=styled.h1({
-  color:'red',
-  border: '1px solid green',
-  borderRadius: '5px',
-  margin:'10px',
-  padding:'20px',
-  })
-
-  const StyleBtn=styled.button`
-  color:red;
-  width:130px;
-  height:40px;
-  margin:10px;
-  `
   return (
     <div>
-    <h1>Styled Component with React js</h1>
-    <Heading>Hello Heading</Heading>
-    <Heading>Hello Heading 2</Heading>
-    <Heading>Hello Heading 3</Heading>
-    <Heading>Hello Heading 4</Heading>
-    <StyleBtn>Login</StyleBtn>
-    <StyleBtn>Sgin Up</StyleBtn>
-    <StyleBtn>Back</StyleBtn>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+      <Alert variant='success'>Hello, BT installed</Alert>
+      
+      <Button onClick={()=>alert("Bootstrap Button")} variant='danger'>Bootstrap Button</Button>
+      <Button variant='warning'>Ok</Button>
+      <Button variant='info'>Ok</Button>
+      <button onClick={()=>alert("Simple Button")}>Simple Button</button>
     </div>
   )
 }
