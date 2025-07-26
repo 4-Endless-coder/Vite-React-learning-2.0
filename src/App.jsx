bf52d834ac1c6d33269a9aee7059ed6dfe625e6b
4-Endless-coder <asheshdash4@gmail.com>
2025-07-26 20:42:12 +0530

    updated ForwardRed in React 19v

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import UserInput from './UserInput';
 
 const App = () => {
+  const inputRef= useRef(null)
+
+  const updateInput=()=>{
+    inputRef.current.value=200;
+    inputRef.current.focus();
+    inputRef.current.style.color='red'
+    
+  }
   return (
     <div>
-      <h1>ForwardRef In react</h1>
+      <h1>Forward Ref</h1>
+        <UserInput ref={inputRef}/>
+        <button onClick={updateInput}>UpdateRef</button>
     </div>
   )
 }
