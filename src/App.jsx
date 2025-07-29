43e051fe74a951f1b5ae925853857f4ee36238fd
4-Endless-coder <asheshdash4@gmail.com>
2025-07-29 12:34:11 +0530

    useTransition Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { startTransition, useState, useTransition } from 'react'
 
 const App = () => {
+  // const [pending, setPending] = useState(false)
+
+  // const handleButton =async ()=>{
+  //   setPending(true)
+  // await new Promise(res=>setTimeout(res, 2000))  
+  //   setPending
+  // }
+const [pending, startTransition] = useTransition();
+
+const handleButton=()=>{
+  startTransition(async ()=>{
+    await new Promise(res=>setTimeout(res, 5000))
+  })
+}
   return (
     <div>
       <h1>useTransition Hook</h1>
+      {/* <button disabled={pending} onClick={handleButton}>Click</button> */}
+
+      {
+        pending ?
+        <img style={{width:'100px'}}src="https://media.tenor.com/joLYNfFQGDgAAAAi/loading.gif" alt="" />
+        :null
+      }
+      <button disabled={pending} onClick={handleButton}>Click</button>
     </div>
   )
 }
