fa0c5124e878502ea78ae85dbc9dd1115f140244
4-Endless-coder <asheshdash4@gmail.com>
2025-09-18 12:27:07 +0530

    useActionState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,42 @@
-import React, { useState } from 'react'
+import React, { useActionState } from 'react'
 
 const App = () => {
-  const [data, setData]=useState([
-    'ahsesh', 'sam', 'peter','john'
-  ])
 
-  const [dataDetails, setDataDetails]=useState([
-      {name: 'ashesh', age: 25},
-      {name: 'sam', age: 27},
-      {name: 'peter', age: 20},
-
-  ])
-
-  const handleUser=(name)=>{
-    
-    data[data.length-1]=name
-    setData([...data])
+  const handleSubmit=async (previousData, formData)=>{
+    let name=formData.get('name');
+    let password=formData.get('password');
+    await new Promise(res=>setTimeout(res, 2000))
+    // console.log('handleSubmit called', name, password);
+    if(name && password){
+      return {messeage: 'Data Submited', name, password}
+    } else {
+      return {error: 'Failed to submit, please try again', name, password}
+    }
     
   }
-  const handleAge=(age)=>{
-    
-    dataDetails[dataDetails.length-1].age=age
-    setDataDetails([...dataDetails])
-    
-  }
+  const [data, action, pending]=useActionState(handleSubmit, undefined)
+  console.log(data);
+  
   return (
     <div>
-      <h1>Updating Array in State</h1>
-      <input type="text" placeholder='Enter lasr user name'
-      onChange={(e)=>handleUser(e.target.value)}
-      />
-
+      <h1>useActionState Hook in React js</h1>
+      <form action={action}>
+        <input defaultValue={data?.name} type="text" placeholder='enter name' name='name' />
+        <br /><br />
+        <input defaultValue={data?.name} type="password" placeholder='enter password' name='password'/>
+        <br /><br />
+        <button disabled={pending}>Submit</button>
+      </form>
       {
-        data.map((item, index)=>(
-          <h2 key={index}>{item}</h2>
-        ))
-      }
-      <hr />
-
-      <input type="text" placeholder='Enter lasr user age'
-      onChange={(e)=>handleAge(e.target.value)}
-      />
-      {
-        dataDetails.map((item, index)=>(
-          <h4 key={index}>{item.name}, {item.age}</h4>
-        ))
-      }
+          data?.error && <span style={{color:'red'}}>{data?.error}</span>
+        }
+        <br />
+          {
+          data?.messeage && <span style={{color:'green'}}>{data?.messeage}</span>
+        }
+        {/* feature test */}
+        <h3>Name : {data?.name}</h3>
+        <h3>Password : {data?.password}</h3>
     </div>
   )
 }
