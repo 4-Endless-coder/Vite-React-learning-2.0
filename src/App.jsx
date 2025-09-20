2b6c14ea69d953c4b7ac590662ae9f86cdcb409f
4-Endless-coder <asheshdash4@gmail.com>
2025-09-20 21:00:07 +0530

    Fragment in React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
-import React, { useId } from 'react'
+import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <UserForm/>
-      <hr />
-      <UserForm/>
-    </div>
+    <>
+      <Data />
+      <Data />
+      <Data />
+      <Data />
+      <Data />
+    </>
+  )
+}
+
+const Data = () => {
+  return (
+    <>
+    <h1>Fragment In React Js</h1>
+    </>
   )
 }
 
 export default App
-
-export const UserForm = () => {
-  const user = useId()
-  // const password = useId()
-  // const skill = useId()
-  // const terms = useId()
-  return (
-    <div>
-      <form action="">
-        <label htmlFor={user+"name"}>Enter Username</label>
-        <input id={user+"name"} type="text" placeholder='enter name' />
-        <br /><br />  
-        <label htmlFor={user+"password"}>Enter password</label>
-        <input id={user+"password"} type="password" placeholder='enter password' />
-        <br /><br />
-        <label htmlFor={user+"skill"}>Enter skill</label>
-        <input id={user+"skill"} type="text" placeholder='enter skill' />
-        <br /> <br />
-        <label htmlFor={user+"terms"}>Terms and Conditions</label>
-        <input id={user+"terms"} type="checkbox" placeholder='enter skill' />
-      </form>
-    </div>
-  )
-}
