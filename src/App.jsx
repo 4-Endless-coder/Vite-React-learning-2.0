9306f6dddcd3dffe11ae464458a47eff2635adc5
4-Endless-coder <asheshdash4@gmail.com>
2025-09-21 22:33:41 +0530

    Router 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
 import React from 'react'
-import useToggle from './useToggle'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 
 const App = () => {
-  const [value, toggleValue] = useToggle(true) 
-
-  const [data, setData] = useToggle(true) 
-  // console.log('val---', value);
-  
   return (
-    <div>
-      <button onClick={toggleValue}>Toggle Head</button>
-      <button onClick={()=>toggleValue(false)}>Hide Heading</button>
-      <button onClick={()=>toggleValue(true)}>Show Heading</button>
-      {
-        value? <h1>Custom Hooks</h1>: null
-      }
-      <hr />
-      <button onClick={setData}>Toggle Head</button>
-      <button onClick={()=>setData(false)}>Hide Heading</button>
-      <button onClick={()=>setData(true)}>Show Heading</button>
-
-      {
-        data? <h1>Second head</h1> : null
-      }
-      
-    </div>
+    <>
+    <Link to="/" >Home</Link>
+    <br />
+    <Link to="about" >About</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   )
 }
 
