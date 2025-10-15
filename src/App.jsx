72324afc6d23112119ee526c9d71fb7d93202408
4-Endless-coder <asheshdash4@gmail.com>
2025-10-15 12:11:31 +0530

    Fetch data from API with Get Method

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,37 @@
-import React from "react";
-import NavBar from "./NavBar";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Login from "./Login";
-import College from "./College";
-import Student from "./Student";
-import Department from "./Department";
-import Details from "./Details";
-import PageNotFound from "./PageNotFound";
-import UserList from "./UserList";
-import UserDetail from "./UserDetail";
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [usersData, setUsersData] = useState([]);
+  useEffect(()=>{
+    getUsersData();
+  }, [])
+  const getUsersData= async()=> {
+    const url = "https://dummyjson.com/users";
+    let response = await fetch(url)
+    response = await response.json()
+    setUsersData(response.users)
+  }
+  console.log(usersData);
+  
   return (
     <>
-      {/* <NavBar /> */}
-      <Routes>
-        <Route element={<NavBar />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/users/list?" element={<UserList/>}/>
-
-          <Route path="/users/:id/:name?" element={<UserDetail />}/>
-
-          <Route path="in">
-            <Route path="/in/user">
-              <Route path="/in/user/login" element={<Login />} />
-              <Route path="/in/user/about" element={<About />} />
-            </Route>
-          </Route>
-        </Route>
-
-        <Route path="/college" element={<College />}>
-          <Route index element={<Student />} />
-          <Route path="department" element={<Department />} />
-          <Route path="details" element={<Details />} />
-        </Route>
-        <Route path="/*" element={<PageNotFound />} />
-        {/* <Route path="*" element={<Navigate to="/login" />} /> */}
-      </Routes>
+      <h1>Ftech data from API</h1>
+      <ul className='user-list user-list-header'>
+            <li>First Name</li>
+            <li>Last Name</li>
+            <li>Age</li>
+          </ul>
+      {
+        usersData && usersData.map((user)=>(
+          <ul className='user-list'>
+            <li>{user.firstName}</li>
+            <li>{user.lastName}</li>
+            <li>{user.age}</li>
+          </ul>
+        ))
+      }
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
