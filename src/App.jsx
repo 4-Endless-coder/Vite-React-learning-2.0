172bef21647b147f783bfb7806b24db4b7b4dc1a
4-Endless-coder <asheshdash4@gmail.com>
2025-11-02 21:07:28 +0530

    Simple Validation in React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import { Routes, Route, NavLink } from 'react-router-dom'
-import UserList from './UserList'
-import UserAdd from './UserAdd'
-import UserEdit from './UserEdit'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [name, setName]= useState("");
+  const [nameErr, setNameErr]= useState();
+  const [password, setPassword]= useState("");
+  const [passErr, setPassErr]= useState();
+
+  const handleName=(e)=>{
+    console.log(e.target.value);
+    if(e.target.value.length > 5){
+      setNameErr("Please Enter Name less than 5 characters");
+    }else{
+      setNameErr();
+    }
+  }
+
+    const handlePassword=(e)=>{
+    let regex= /^[A-Z0-9]+$/i;
+    if(regex.test(e.target.value)){
+      setPassErr();
+      
+    }else{
+      setPassErr("Password must be alphanumeric");
+    }
+  }
+
   return (
     <>
-    <ul className='nav-list'>
-      <li>
-        <NavLink to="/">List</NavLink>
-      </li>
-      <li>
-        <NavLink to="/add">Add User</NavLink>
-      </li>
-    </ul>
-
-      <Routes>
-        <Route path="/" element={<UserList />} />
-        <Route path="/add" element={<UserAdd />} />
-        <Route path="/edit/:id" element={<UserEdit />} />
-      </Routes>
+      <input className={nameErr?'error':''} type="text" onChange={handleName} placeholder='Enter Name' />
+      <span className='red-color'>{nameErr && nameErr}</span>
+      <br /><br />
+      <input className={passErr?'error':''} type="text" onChange={handlePassword} placeholder='Enter Password' />
+      <span className='red-color'>{passErr && passErr}</span>
+      <br /><br />
+      <button disabled={passErr || nameErr}>Login</button>
     </>
   )
-}
+ }
 
-export default App
+export default App;
