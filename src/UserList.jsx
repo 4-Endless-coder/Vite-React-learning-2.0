da84146b97964ab887b3f85e5122398c422baee2
4-Endless-coder <asheshdash4@gmail.com>
2025-11-02 12:49:37 +0530

    API for Delete Method

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -3,25 +3,37 @@ import React, { useEffect, useState } from 'react'
 const UserList = () => {
   const [userData, setUserData]= useState([])
   const [loading, setLoading]= useState(false)
+  const url = "http://localhost:3000/users"
   useEffect(()=>{
     setLoading(true);
     getUserData();
   }, [])
 
   const getUserData =async()=> {
-    const url = "http://localhost:3000/users"
     let response = await fetch(url);
     response =await response.json();
     console.log(response);
     setUserData(response)
     setLoading(false);
   }
+
+  const deleteUser=async(id)=>{
+    let response = await fetch(url+"/"+id,{
+      method:'delete',
+    })
+    response =await response.json();
+    if(response){
+      alert('user deleted')
+      getUserData();
+    }
+  };
   return (
     <>
         <ul className='user-list user-list-header'>
             <li>Name</li>
             <li>Age</li>
             <li>Email</li>
+            <li>Action</li>
           </ul>
       
       {
@@ -31,6 +43,7 @@ const UserList = () => {
             <li>{user.name}</li>
             <li>{user.age}</li>
             <li>{user.email}</li>
+            <li><button onClick={()=>deleteUser(user.id)}>Delete</button></li>
           </ul>
         ))
         :<h2>Data Loading...</h2>
