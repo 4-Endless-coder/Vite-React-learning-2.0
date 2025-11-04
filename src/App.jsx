032964f112bf6bcc57e418f4d11705393f4c9c4d
4-Endless-coder <asheshdash4@gmail.com>
2025-11-04 21:37:56 +0530

    useReducer Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
-import React, { useActionState } from 'react'
+import React, { useReducer } from 'react'
 
-const App = () => {
-  const handleLogin =(prevData, formData)=>{
-    let name = formData.get('name')
-    let password = formData.get('password')
-    let regex =/^[A-Z0-9]+$/i;
+const emptyData ={
+    name:'',
+    password:'',
+    email:'',
+    city: '',
+    address: '',
+  }
 
-    if(!name || name.length > 5){
-      return{error:'Name should be 5 charchters', name, password}
-    }else if(!regex.test(password)){
-      return{error:'Password can container only alpha or numeric', name, password}
-    }else{
-      return{message:'Login Success', name, password}
-    }
-
-    // console.log(name, password);
+  const reducer= (data,action)=>{
+    return{...data,[action.type]:action.val}
     
   }
-  const [data, action,pending]= useActionState(handleLogin);
-  
+
+const App = () => {
+  const [state,dispatch]=useReducer(reducer,emptyData);
   
   return (
     <>
-      <h1>Validation with useActionState</h1>
-      {
-        data?.message && <span style={{color:'green'}}>{data?.message}</span>
-      }
-      {
-        data?.error && <span style={{color:'red'}}>{data?.error}</span>
-      }
-      <form action={action}>
-        <input defaultValue={data?.name} type="text" name='name' placeholder='enter user name' />
-        <br /><br />
-        <input defaultValue={data?.password} type="text" name='password' placeholder='enter user password' />
-        <br /><br />
-        <button>Login</button>
-      </form>
+      <h1>Use Reducer</h1>
+      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'name'})} placeholder='enter name' />
+      <br /><br />
+      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'password'})} placeholder='password' />
+      <br /><br />
+      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'email'})} placeholder='enter email' />
+      <br /><br />
+      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'city'})} placeholder='enter city' />
+      <br /><br />
+      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'address'})} placeholder='enter address' />
+      <br /><br />
+
+      <ul>
+        <li>Name:{state.name}</li>
+        <li>password:{state.password}</li>
+        <li>email:{state.email}</li>
+        <li>city:{state.city}</li>
+        <li>address:{state.address}</li>
+      </ul>
+      <button onClick={()=>console.log(state)}>Add deatils</button>
     </>
   )
 }
