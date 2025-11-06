539e0b457abcc8390cc4a17133e9d24aeb2632d1
4-Endless-coder <asheshdash4@gmail.com>
2025-11-06 13:23:30 +0530

    useOptimistic

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,55 @@
-import React, { Suspense, use } from "react";
+import { useOptimistic, useState, useRef, useEffect } from "react";
 
-const fetchData = () =>
-  fetch("https://dummyjson.com/users").then((response) => response.json());
-// console.log(fetchData());
-const userResource =fetchData();
 
-const App = () => {
+export default function App() {
+
+  const [skills, setSkills] = useState([]);
+  const [name, setName] = useState([]);
+  const [optSkills,setOptSkills]=useOptimistic(skills)
+  useEffect(() => {
+    getSkills()
+  }, []);
+
+  const getSkills = async () => {
+    let resp = await fetch('http://localhost:3000/skills');
+    resp = await resp.json();
+    setSkills(resp)
+  }
+
+  function sleep(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms))
+  }
+  const addSkill = async (event) => {
+
+   
+    const id = Math.random() * 100000;
+    setOptSkills((prev)=>[...prev,{name,id}])
+    let resp = await fetch('http://localhost:3000/skills', {
+      method: "post",
+      body: JSON.stringify({ name, id })
+    });
+    await sleep(3000)
+    resp = await resp.json();
+    
+    console.log("apple", resp);
+
+    if (resp) {
+      getSkills()
+    }
+  }
+
+
   return (
-    <>
-      <h1>use API in React</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Users userResource={userResource} />
-      </Suspense>
-    </>
-  );
-};
-
-const Users = ({userResource}) => {
-  // console.log(userResource);
-  const userData =use(userResource);
-  console.log(userData.users);
-  
-  return (
-    <>
-      <h1>Users List</h1>;
+    <div>
+<form action={addSkill}>
+      <input type="text" onChange={(e) => setName(e.target.value)} placeholder="enter skill" />
+      <button  >Add </button>
+      </form>
       {
-        userData?.users?.map((user)=>(
-          <h1>{user.firstName}</h1>
+        optSkills.map((item) => (
+          <div key={item.id} >{item.name}</div>
         ))
       }
-    </>
+    </div>
   )
-};
-
-export default App;
+}
