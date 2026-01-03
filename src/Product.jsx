597fb49acacce31d2e1ed116ce79b5e495dab62b
4-Endless-coder <asheshdash4@gmail.com>
2026-01-03 21:52:09 +0530

    Display Product Listing

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,27 +2,32 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AddToCart from "./AddToCart";
 import { addItem, removeItem } from "./redux/slice";
-import {fetchProducts} from './redux/productSlice'
+import { fetchProducts } from "./redux/productSlice";
 
 const Product = () => {
-    const dispatch =useDispatch()
-    useEffect(()=>{
-      dispatch(fetchProducts())
-    },[])
-    const productSelector =useSelector((state)=>state.products.items)
-    console.log(productSelector);
-    
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+  const productSelector = useSelector((state) => state.products.items);
+  console.log(productSelector);
 
   return (
     <div>
       <div className="grid">
-        {
-          productSelector.length && productSelector.map((item)=>(
-            <div className="card">
-              <img src={item.thumbnail} alt="" />
+        {productSelector.length &&
+          productSelector.map((item) => (
+            <div className="card" key={item.id}>
+              <img src={item.thumbnail} />
+              <div className="content">
+                <div className="title">{item.title}</div>
+                <div className="brnad">{item.brnad}</div>
+                <div className="price">{item.price}</div>
+                <div className="rating">{item.rating}</div>
+                <button className="add-to-cart">Add to Cart</button>
+              </div>
             </div>
-          ))
-        }
+          ))}
       </div>
     </div>
   );
